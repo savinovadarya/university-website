71f0cfe67c6ea34afa24674e108dbe0d884b4fef
Daria Savinova <drsvnv77@gmail.com>
2022-10-01 11:20:58 +0400

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,9 @@
 		</div>
 		<nav class="header__nav nav">
 			<ul class="nav__list list">
-				<li class="list__item"><a href="#">About</a></li>
-				<li class="list__item"><a href="#">Academics</a></li>
-				<li class="list__item"><a href="#">Contact</a></li>
+				<li class="list__item"><a class="list__link" href="#">About</a></li>
+				<li class="list__item"><a class="list__link" href="#">Academics</a></li>
+				<li class="list__item"><a class="list__link" href="#">Contact</a></li>
 			</ul>
 		</nav>
 	</header>
@@ -21,7 +21,7 @@
 	import Icon from './Icon.vue'
 
 	export default {
-		components: { Icon, }
+		components: { Icon }
 	}
 </script>
 
@@ -31,42 +31,58 @@
 		flex-direction: row;
 		align-items: center;
 		justify-content: space-between;
-		&__logo {
-		}
-
-		&__nav {
-		}
+		padding: 24px 24px 24px 24px;
+		// &__logo {
+		// }
+		// &__nav {
+		// }
 	}
 	.logo {
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		&__image {
+			margin: 0px 16px 0px 0px;
 		}
-
 		&__title {
 			display: flex;
 			flex-direction: column;
 		}
 	}
 	.logo-title {
-
 		&__sup {
 			font-size: 24px;
 			font-weight: 600;
+			margin: 0px 0px 4px 0px;
 		}
-
-		&__sub {
-		}
+		// &__sub {
+		// }
 	}
-	.nav {
-
-		&__list {
-		}
-		}
-		.list {
-
+	// .nav {
+	// 	&__list {
+	// 	}
+	// }
+	.list {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
 		&__item {
+			font-size: 17px;
+			font-weight: 700;
+			color: var(--dark-grey);
+			margin: 0px 48px 0px 0px;
+			&:last-child {
+				margin: 0px 0px 0px 0px;
+			}
+		}
+		&__link {
+			color: var(--dark-grey);
+			text-decoration: none;
+			transition: color .2s ease-in-out;
+			&:hover {
+				color: var(--black);
+			}
 		}
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './styles/main.css'
+import './styles/style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
